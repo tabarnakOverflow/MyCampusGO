bf6f75597b3a9d20d2f6fda0f187249bfec38200
Jonas Tkacz <jonas@tkacz.ca>
2026-01-27 20:31:14 -0400

    Add StFX colours and a Material 2 top bar,

--- mycampusgo_client/app/src/main/java/ca/tkacz/mycampusgo/MainActivity.kt
+++ mycampusgo_client/app/src/main/java/ca/tkacz/mycampusgo/MainActivity.kt
@@ -2,10 +2,30 @@ package ca.tkacz.mycampusgo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val appBar = findViewById<AppBarLayout>(R.id.appBarLayout)
+
+        ViewCompat.setOnApplyWindowInsetsListener(appBar) { v, insets ->
+            val topInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            v.updatePadding(top = topInset)
+            insets
+        }
+
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(toolbar)
     }
+
 }
